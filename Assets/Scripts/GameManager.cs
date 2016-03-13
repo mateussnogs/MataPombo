0d7af297c17cec154e2127ae7064f27370266626
mateussnogs <mateussnogs@gmail.com>
2016-03-13 14:01:16 -0300

    Matar inimigos ta direito agora.
    Pode ter varios spawners

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 public class GameManager : MonoBehaviour {
     public enum TipoFigura { HOR, VER, V};
     public float tempoEspera, proxFig;
     TipoFigura tipoFig = TipoFigura.HOR;
-    public Text textoFiguraDaVez;
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public static List<ParEnemyBalao> filaInimigos;
+    // Use this for initialization
+    void Start () {
+        filaInimigos = new List<ParEnemyBalao>();
+    }
 	
 	// Update is called once per frame
 	void Update () {        
